04e4421b25d5438c4d2f3d40a8b7bc61d38233ad
Andrew Markham <andrewjmarkham1@gmail.com>
2020-03-08 17:30:34 +0000

    determines which numbers between 100 - 200 are divisible by 3 or 4

---
+++ loopy-lighthouse.js
@@ -0,0 +1,11 @@
+for (let x = 100; x <= 200; x++) {
+  if ((x % 3 === 0) && (x % 4 !== 0)) {
+    console.log("Loopy");
+  } if ((x % 4 === 0) && (x % 3 !== 0)) {
+    console.log("Lighthouse");
+  } if ((x % 4 === 0) && (x % 3 === 0)) {
+    console.log("LoopyLighthouse");
+  } else if ((x % 4 !== 0) && (x % 3 !== 0)) {
+    console.log(x);
+  }
+}
